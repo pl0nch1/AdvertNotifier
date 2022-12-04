5bcc18fa0940b22b1d2c13fc6df5cb6fd558b69b
pl0nch1 <yan-77777@ya.ru>
2022-12-04 23:05:28 +0300

    Conversation test

--- app/telegram/telegram_service.py
+++ app/telegram/telegram_service.py
@@ -1,61 +1,76 @@
 import asyncio
+import logging
 
-from telethon import events, TelegramClient
+from telethon import events, TelegramClient, types, Button
+from telethon.events import NewMessage
+from telethon.events.common import EventBuilder
+from telethon.tl.types import KeyboardButton
+
 from app.context import user_manager, is_admin
 from app.errors import OutOfQuotaException, UserUnsubscribed, UserHasNoRequests
+
+logger = logging.getLogger('messages_handler')
+logger.setLevel(logging.INFO)
+
+
+async def handle_subscribe(client: TelegramClient, event, arg):
+    sender = await event.get_sender()
+    async with client.conversation(await event.get_sender()) as conv:
+        if not arg:
+            buttons = client.build_reply_markup([
+                Button.inline('Подписаться на месяц 1 месяц', 1),
+                Button.inline('Подписаться на 3 месяца', 3)
+            ])
+            message = await client.send_message(sender, 'Оформить подписку можно в любое время с помощью онлайн кассы',
+                                                buttons=buttons)
+            arg = str((await conv.wait_event(events.CallbackQuery(sender.username))).data)
+            await client.delete_messages(sender, [message.id])
+        await client.send_message(sender, f'Оформляем подписку на {arg} месяцев(-а)...')
+
+
+async def show_help(client, user_id):
+    text = '\n'.join(handlers.keys())
+    await client.send_message(user_id, text)
 
 
 def start_telegram(client: TelegramClient):
     with client:
-        @client.on(events.NewMessage(pattern='(?i)hi|hello'))
-        async def handler(event):
-            await event.respond(str((await event.get_sender()).username))
-
-        @client.on(events.NewMessage(pattern='(?i)/subscribe .+'))
-        async def subscribe_handler(event):
-            sender_id = (await event.get_sender()).username
-            if not is_admin(sender_id):  # TODO: add admin check decorator
-                await event.respond('You are not admin')
-                return
-            tg_id = event.raw_text.split(' ')[1]
-            n_requests = int(event.raw_text.split(' ')[2])
-            user_manager.subscribe(tg_id, n_requests)
-            await event.respond(f'Subscribed {tg_id} for {n_requests} requests')
-
-        @client.on(events.NewMessage(pattern='(?i)/add .+'))
-        async def add_request_handler(event):
-            sender_id = (await event.get_sender()).username
-            req = event.raw_text.split(' ')[1]
-
-            try:
-                user_manager.append_request(sender_id, req)
-                await event.respond('Запрос добавлен')
-            except OutOfQuotaException as e:
-                await event.respond("У вас закончились запросы")
-            except UserUnsubscribed as e:
-                await event.respond("У вас закончилась подписка")
-
-        @client.on(events.NewMessage(pattern='(?i)/remove .+'))
-        async def remove_request_handler(event):
-            sender_id = (await event.get_sender()).username
-            ind = int(event.raw_text.split(' ')[1]) - 1
-            if isinstance(ind, int):
-                user_manager.release_request(sender_id, ind)
-                await event.respond('Запрос удален')
+        @client.on(events.NewMessage())
+        async def handle(event):
+            text = event.message.message.strip()
+            logger.info(f'Got {text} from {await event.get_sender()}')
+            if ' ' not in text:
+                command = text
+                arg = None
             else:
-                await event.respond('Неверный номер запроса')
+                space_index = text.index(' ')
+                command = text[:space_index:]
+                arg = text[space_index+1::]
+            handler = handlers.get(command)
+            if not handler:
+                await event.respond('Команда не распознана')
+                await show_help(client, (await event.get_sender()).id)
+            else:
+                await handler(client, event, arg)
 
-        @client.on(events.NewMessage(pattern='(?i)/list'))
-        async def list_requests_handler(event):
-            sender_id = (await event.get_sender()).username
+        @client.on(events.NewMessage(pattern='/test'))
+        async def test(event):
+            buttons = client.build_reply_markup([
+                Button.text('Оформить подпсику'),
+                Button.text('Добавить мониторинг')
+            ])
+            await client.send_message((await event.get_sender()).username, 'hi', buttons=buttons)
 
-            res = ''
-            try:
-                for ind, req in enumerate(user_manager.list_requests(sender_id)):
-                    res += f'{ind + 1}: {req}\n'
-                await event.respond(res, link_preview=False)
-
-            except UserHasNoRequests as e:
-                await event.respond('У вас нет запросов')
+        @client.on(events.NewMessage(pattern='/test1'))
+        async def test(event):
+                await conv.send_message('hi')
+                answer = await conv.get_response()
+                print(answer)
+                await conv.send_message(f'Hi {answer}')
 
         client.run_until_disconnected()
+
+
+handlers = {
+    '/subscribe': handle_subscribe
+}
